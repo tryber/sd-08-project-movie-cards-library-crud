238afe4bc41c0a0f7c8d06c7a487acff1ae4c73d
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-26 16:37:37 -0300

    Req01: dender BrowserRouter inside App component using Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="*" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
