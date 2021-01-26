60980e2b1e330f714003fd3ed57bec6166dd8796
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-26 19:59:53 -0300

    Partial implementation of Req2

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ MovieList } />
         <Route exact path="/movies/new" component={ NewMovie } />
-        <Route exact path="/movies/:id" component={ MovieDetails } />
         <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
         <Route exact path="*" component={ NotFound } />
       </Switch>
     </BrowserRouter>
