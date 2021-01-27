dccaec89b3e357fd7f614a3059e9665afea454ce
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-01-27 00:01:00 -0300

    req1 take5

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={ MovieList } />
         <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/" component={ MovieList } />
         <Route path="" component={ NotFound } />
       </Switch>
       ;
