9e44c78c7d5e8e0bea266b882600b7a2a621b9fa
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-01-27 10:36:59 -0300

    req4 take6

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -14,6 +14,7 @@ class EditMovie extends Component {
       movie: {},
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
   }
 
   componentDidMount() {
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -45,8 +45,8 @@ class MovieDetails extends Component {
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
-        <Link to={ `/movies/${id}/edit"` }> EDITAR </Link>
-        <Link to="/"> VOLTAR </Link>
+        <Link to={ `/movies/${id}/edit"` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
