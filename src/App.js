e36b5e4c09cf071571196e0e468a57969d67825d
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-01-27 01:24:28 -0300

    C3 - Fixing 1st Req

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import MovieList from './pages/MovieList';
@@ -11,11 +11,13 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <Router>
-      <Route path="/" component={ MovieList } />
-      <Route path="/movies/new" component={ NewMovie } />
-      <Route path="/movies/:id" component={ MovieDetails } />
-      <Route path="/movies/:id/edit" component={ EditMovie } />
-      <Route path="/:id" component={ NotFound } />
+      <Switch>
+        <Route path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/:id" component={ NotFound } />
+      </Switch>
     </Router>
   );
 }
