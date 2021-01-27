691dad0addc2f480be1c7462aa3f8f63dda763d0
Digo-Castro <dygo.castro@gmail.com>
2021-01-27 15:08:02 -0300

    Digo-Castro - Project Movie Cards Library - CRUD - Ajusta CSS e add SVG Background

--- src/App.js
+++ src/App.js
@@ -12,13 +12,15 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Switch>
-        <Route exact path="/" component={ MovieList } />
-        <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
-        <Route component={ NotFound } />
-      </Switch>
+      <div className="max-width-screen">
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route component={ NotFound } />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
