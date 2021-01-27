356d5260fd8b3c65ef419f0f794689915f3cce4f
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-01-26 23:23:19 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="" component={ NotFound } />
+      </Switch>
+      ;
+    </BrowserRouter>
   );
 }
 
