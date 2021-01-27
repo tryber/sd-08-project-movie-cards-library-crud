163c86707ce0e15d36576f7fd081b9aa06554552
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-01-27 12:47:11 -0300

    C4 - Fixing 1st Req

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import MovieList from './pages/MovieList';
-import NewMovie from './pages/NewMovie';
-import MovieDetails from './pages/MovieDetails';
-import EditMovie from './pages/EditMovie';
-import NotFound from './pages/NotFound';
+import { MovieList, 
+  NewMovie, 
+  MovieDetails, 
+  EditMovie, 
+  NotFound } from './pages/index';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Switch>
-        <Route path="/" component={ MovieList } />
-        <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
         <Route path="/:id" component={ NotFound } />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
